1bf8761d7be046abbdb6d225b73af9def77b848b
hii24 <130472173+hii24@users.noreply.github.com>
2023-09-26 20:04:49 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+const CLIENT_ID = '1205582848933137'
+const CLIENT_SECRET = '3657eb161045ee2385959c0f7a882c99'
 function App() {
+  const [code, setCode] = useState<string>('');
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const codeFromUrl = params.get("code");
+
+    if (codeFromUrl) {
+      setCode(codeFromUrl);
+    }
+  }, []);
+
+  const handlePostClick = async () => {
+    if (code) {
+      try {
+        const data = {
+          grant_type: "authorization_code",
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          redirect_uri: "https://t11est.kyiv.ua/",
+          code: code,
+        };
+
+        const response = await axios.post(
+          "https://app.asana.com/-/oauth_token",
+          data
+        );
+
+        const tokenData = response.data;
+        console.log("Токен Asana:", tokenData);
+      } catch (error) {
+        console.error(
+          error
+        );
+      }
+    } 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <>
+    <h1>Test</h1>
+      {code ? (
+        <>
+          <p>code: {code}</p>
+          <button onClick={handlePostClick}>POST</button>
+        </>
+      ) : (
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={`https://app.asana.com/-/oauth_authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=https%3A%2F%2Ft11est.kyiv.ua%2F`}
         >
-          Learn React
+          Authenticate with Asana
         </a>
-      </header>
-    </div>
+      )}
+    </>
   );
 }
 
